829e8c5793ad80d7165e17dc2e6a6a130922186d
Ilya Sotkov <ilya@sotkov.com>
2018-03-24 03:44:46 +0200

    Update scripts/tls-backup.sh script paths

--- scripts/tls-backup.sh
+++ scripts/tls-backup.sh
@@ -4,11 +4,11 @@ domain_zone="flexeption.pw"
 source=("ci" "registry" "charts" "staging" "production")
 green="\e[1;32m"
 no_color="\e[0m"
-secret_file_name="secret-$(date '+%Y-%m-%d-%H-%M-%S').yaml"
+secret_file_name="certs-$(date '+%Y-%m-%d-%H-%M-%S').yaml"
 
 for i in ${source[@]}
 do
-    echo "---" >> /project/live/prod/secrets/kube-system/${secret_file_name} \
-    && kubectl get secret $i.$domain_zone-tls -o yaml >> /project/live/prod/secrets/team1/${secret_file_name} \
-    && printf "${green}Secret $i.$domain_zone-tls has been saved to ./live/prod/secrets/team1/${secret_file_name} successfully!\n${no_color}"
+    echo "---" >> ${TF_VAR_secrets_dir}/kube-system/kube-lego/${secret_file_name} \
+    && kubectl get secret $i.$domain_zone-tls -o yaml >> ${TF_VAR_secrets_dir}/kube-system/${secret_file_name} \
+    && printf "${green}Secret $i.$domain_zone-tls has been saved to ${TF_VAR_secrets_dir}/kube-system/${secret_file_name} successfully!\n${no_color}"
 done
